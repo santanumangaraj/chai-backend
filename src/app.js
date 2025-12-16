ba6648a8851e0dca9ea1a8d6429b2be9b1fc8f58
Santanu Mangaraj <santanumangaraj80@gmail.com>
2025-12-16 13:16:08 +0530

    add user & video model

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 const app = express()
 
+//use while using middleware or configurtion settings
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
@@ -12,9 +13,9 @@ app.use(cors({
 
 app.use(express.json({
     limit: "16kb"
-}))
+}))//for parsing application/json
 
-app.use(express.urlencoded({extended: true,limit: "16kb"}))
+app.use(express.urlencoded({extended: true,limit: "16kb"}))//for parsing application/x-www-form-urlencoded or url encoded
 app.use(express.static("public"))
 app.use(cookieParser())
 

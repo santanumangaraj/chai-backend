5bf954916f08a837b827769984621f06c9aa42b3
Santanu Mangaraj <santanumangaraj80@gmail.com>
2025-12-30 11:36:54 +0530

    Implement user authentication with login, logout, and token refresh functionality

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -40,19 +40,20 @@ const userSchema = new Schema({
         type: String,
         required: [true, "Password is required"],
     },
-    refresToken:{
+    refreshToken:{
         type: String,
     }
 },{
     timestamps: true,
 });
 
-userSchema.pre("save",async function(next){
-    if(!this.isModified("password")) return next();
 
-    this.password = await bcrypt.hash(this.password,10)
-    next
-})
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
+
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)

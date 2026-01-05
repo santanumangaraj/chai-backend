52ac88fdb4d5b19d7eed80e392ba427026bf52bc
Santanu Mangaraj <santanumangaraj80@gmail.com>
2026-01-05 21:22:36 +0530

    feat: add models for comments, likes, playlists, and tweets

---
+++ src/models/like.model.js
@@ -0,0 +1,25 @@
+import mongoose,{Schema} from "mongoose";
+
+const likeSchema = new Schema({
+    comment:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+    video:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    likedby:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    tweet:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Tweet"
+    }
+    
+},{
+    timestamps:true
+})
+
+export const Like = mongoose.model("Like", likeSchema)

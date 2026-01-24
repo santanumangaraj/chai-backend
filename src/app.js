99b264fd4d386418f133f602eb3edf252f4807f1
Santanu Mangaraj <santanumangaraj80@gmail.com>
2026-01-24 21:09:00 +0530

    feat: add dashboard routes and controller for channel statistics and videos

--- src/app.js
+++ src/app.js
@@ -29,6 +29,8 @@ import tweetRouter from "./routes/tweet.routes.js"
 import commentRouter from "./routes/comment.routes.js"
 import likeRouter from "./routes/like.routes.js"
 import playlistRouter from "./routes/playlist.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
@@ -36,7 +38,8 @@ app.use("/api/v1/subscriptions",subscriptionRouter)
 app.use("/api/v1/tweets",tweetRouter)
 app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/likes",likeRouter)
-app.use("/api/v1/playlists",playlistRouter)
-
+app.use("/api/v1/playlist",playlistRouter)
+app.use("/api/v1/dashboard",dashboardRouter)
+app.use("/api/v1/healthcheck",healthcheckRouter)
 
 export {app}
---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,88 @@
+import {Video} from "../models/video.model.js";
+import {Like} from "../models/like.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import {ApiError} from "../utils/ApiError.js";
+import {ApiResponse} from "../utils/ApiResponse.js";
+import {asyncHandler} from "../utils/asyncHandler.js";
+
+
+const getChannelStats = asyncHandler(async (req , res)=>{
+    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
+    const userId = req.user?._id
+
+    if(!userId){
+        throw new ApiError(404,"User not found")
+    }
+
+    const videoStats = await Video.aggregate([
+        {
+            $match:{
+                owner:userId
+            }
+        },
+        {
+            $lookup:{
+                from:"likes",
+                localField:"_id",
+                foreignField:"video",
+                as:"likes"
+            }
+        },
+        {
+            $group:{
+                _id:null,
+                totalVideos:{$sum: 1},
+                totalViews:{$sum:"$views"},
+                totalLikes:{$sum:{$size:"$likes"}}
+            }
+        }
+
+    ])
+
+    
+
+    const totalSubscribers = await Subscription.countDocuments({
+        channel:userId
+    })
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200,
+            {
+                totalVideos: videoStats[0]?.totalVideos || 0,
+                totalViews: videoStats[0]?.totalViews || 0,
+                totalLikes: videoStats[0]?.totalLikes || 0,
+                totalSubscribers,
+            }
+        )
+    )
+})
+
+const getChannelVideos = asyncHandler(async (req , res)=>{
+    // TODO: Get all the videos uploaded by the channel
+    const userId = req.user?._id
+
+    if(!userId){
+        throw new ApiError(400,"User id doesn't exists")
+    }
+
+    const getVideos = await Video.find({owner:userId})
+
+    if(!getVideos){
+        throw new ApiError(404,"No videos found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            getVideos,
+            "Channel videos fetched successfully"
+        )
+    )
+
+})
+
+export {getChannelStats,getChannelVideos}

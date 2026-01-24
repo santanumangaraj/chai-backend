2931095d3a2bab454a375fa6cb8e13ebc5f41310
Santanu Mangaraj <santanumangaraj80@gmail.com>
2026-01-24 21:09:12 +0530

    feat: add healthcheck controller and routes for server status monitoring

---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,22 @@
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+
+
+const healthcheck = asyncHandler(async (req,res)=>{
+    return res.
+    status(200)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                status:"OK",
+                uptime: process.uptime(),
+                timestamp: Date.now()
+            },
+            "Server is healthy"
+        )
+    )
+})
+
+export {healthcheck}

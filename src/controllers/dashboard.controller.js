b1faad7235be0a12d4058b9123e2d171fa881a5b
Santanu Mangaraj <santanumangaraj80@gmail.com>
2026-01-24 21:13:35 +0530

    refactor: remove TODO comments from dashboard controller methods

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -7,7 +7,6 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 
 
 const getChannelStats = asyncHandler(async (req , res)=>{
-    // TODO: Get the channel stats like total video views, total subscribers, total videos, total likes etc.
     const userId = req.user?._id
 
     if(!userId){
@@ -39,7 +38,7 @@ const getChannelStats = asyncHandler(async (req , res)=>{
 
     ])
 
-    
+
 
     const totalSubscribers = await Subscription.countDocuments({
         channel:userId
@@ -60,7 +59,6 @@ const getChannelStats = asyncHandler(async (req , res)=>{
 })
 
 const getChannelVideos = asyncHandler(async (req , res)=>{
-    // TODO: Get all the videos uploaded by the channel
     const userId = req.user?._id
 
     if(!userId){
